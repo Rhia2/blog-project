605b9b2c948ccbfb758a7dd4feb8cd64edd2fb95
Adedeji Omobolanle <o.adedeji@concept-nova.com>
2017-08-25 15:49:15 +0100

    install laravel

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-
-    <footer class="blog-footer">
-        <p>Blog built by @Mariam</p>
-        <p>
-            <a href="#">Back to top</a>
-        </p>
-    </footer>
--- resources/views/tasks/show.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-    <html>
-        <head>
-            <title>id checker</title>
-        </head>
-        <body>
-            <h1> {{$task->body }} </h1>
-        </body>
-    </html>
